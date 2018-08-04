404dd9165746f4ed79780d43bdf6ef56cac5055d
Sorin Marian <marian.sorin@yahoo.com>
2018-08-04 14:57:41 +0300

    liskov substitution principle

--- 3_liskov_substitution/wrong/Ostrich.php
+++ 3_liskov_substitution/wrong/Ostrich.php
@@ -5,7 +5,6 @@ class Ostrich implements IBird{
   /**
   *   The ostrich cannot fly. Basically Ostrich class introduces new behavior to the base class Bird.
   *   So it breaks the Liskov Substitution Priciple.
-  *   If we change the Bird class to make some
   *
   */
   public function fly(){
