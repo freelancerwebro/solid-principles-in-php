cc6288b4cadaaaf309e7b9edf6b3f1501cd1bd4c
Sorin Marian <marian.sorin@yahoo.com>
2020-04-20 22:58:01 +0300

    update comment on dependency inversion principle

--- 5_dependency_inversion/BudgetReport.php
+++ 5_dependency_inversion/BudgetReport.php
@@ -4,6 +4,9 @@
  * BudgetReport class depends on an abstraction. It's easier to change the DB system now. 
  * When we create an instance of BudgetReport class, we need to pass an object of PostgreSQL
  * to this class, instead of MySQL.
+ *
+ * As a result, the direction of the original dependency has been inverted: Low-level classes
+ * are now dependent on high-level abstractions.
  */
 class BudgetReport {
     
